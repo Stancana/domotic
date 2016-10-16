8545333cbc2bf6e033df2715cd18e251074fd4b3
Clément Taboulot <clement-taboulot@hotmail.fr>
2016-10-16 18:27:13 +0200

    Design of the page with bootstrap

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 
 app.use('/', routes);
 app.use('/users', users);
@@ -37,6 +38,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
+  app.locals.pretty = true;
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
