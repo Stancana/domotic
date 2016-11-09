6f0ed28f1476095d19cdc356ea55ed36d905e4a3
Vincent Chenal <vince.chenal@gmail.com>
2016-11-09 13:50:03 +0100

    The application now retries connecting to mongoDB if it loses the connection

--- config/mongo_config.js
+++ config/mongo_config.js
@@ -1,5 +1,6 @@
 var exports = {
-    MONGO_URL : "mongodb://mongodb:27017",
+    MONGO_URL : "mongodb://localhost:27017",
+    MONGO_URL_PROD : "mongodb://mongo:27017",
     MONGO_LOGIN : "",
     MONGO_PASSWORD : "",
     MONGO_DBNAME : "fablab"
@@ -9,14 +10,42 @@ var setDBConfig = function(app){
     //Manage connection
     var mongoose = require('mongoose');
     mongoose.Promise = global.Promise;
-    mongoose.connect(exports.MONGO_URL + '/' + exports.MONGO_DBNAME);
+
     var db = mongoose.connection;
-    db.on('error', function () {
-        console.log('Connection to dabatase FAILED');
+
+    db.on('connecting', function() {
+        console.log('connecting to MongoDB...');
     });
-    db.on('open', function () {
-        console.log('Connection to dabatase SUCCESS.')
+
+    db.on('error', function(error) {
+        console.error('Error in MongoDb connection: ' + error);
+        mongoose.disconnect();
     });
+    db.on('connected', function() {
+        console.log('MongoDB connected!');
+    });
+    db.once('open', function() {
+        console.log('MongoDB connection opened!');
+    });
+    db.on('reconnected', function () {
+        console.log('MongoDB reconnected!');
+    });
+    db.on('disconnected', function() {
+        console.log('MongoDB disconnected!');
+        setTimeout(function(){
+            if(process.env.MODE == "PROD"){
+                mongoose.connect(exports.MONGO_URL_PROD + '/' + exports.MONGO_DBNAME, {server:{auto_reconnect:true}});
+            }else{
+                mongoose.connect(exports.MONGO_URL + '/' + exports.MONGO_DBNAME, {server:{auto_reconnect:true}});
+            }
+        },5000);
+
+    });
+    if(process.env.MODE == "PROD"){
+        mongoose.connect(exports.MONGO_URL_PROD + '/' + exports.MONGO_DBNAME, {server:{auto_reconnect:true}});
+    }else{
+        mongoose.connect(exports.MONGO_URL + '/' + exports.MONGO_DBNAME, {server:{auto_reconnect:true}});
+    }
 }
 
 module.exports = {
