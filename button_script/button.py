656e9d77540803a388ca4e9d70b93f45d4593aa9
Vincent Chenal <vince.chenal@gmail.com>
2016-11-18 14:24:39 +0100

    Add sound when push button

--- button_script/button.py
+++ button_script/button.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
 import time
+import pygame
+
 
 GPIO.setmode(GPIO.BCM)
 
@@ -8,5 +10,10 @@ GPIO.setup(18,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 while True:
     input_state = GPIO.input(18)
     if input_state == False:
-        print ("Button Pressed")
-        time.sleep(0.2)
+        print ("Button Pressed")   
+        pygame.mixer.init()
+        pygame.mixer.music.load("Doorbell-ringtone.mp3")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            continue
+        time.sleep(2)
