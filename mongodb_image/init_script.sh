6c174f43861ebc6c26bb9a283820df415de25a63
taboulot <clement-taboulot@hotmail.fr>
2017-01-23 08:48:55 +0100

    change mdp

--- mongodb_image/init_script.sh
+++ mongodb_image/init_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker exec -t mongodb mongo fablab --eval "db.admins.insert({'firstName':'Administrateur', 'lastName':'Administrateur', 'email':'administrateur@administrateur.com', 'password':'sha1\$9eb66656\$1\$531227f8880d64b9ce1c848ff4e0bcabcf7c733f'})"
+docker exec -t mongodb mongo fablab --eval "db.admins.insert({'firstName':'Administrateur', 'lastName':'Administrateur', 'email':'fablab.notifier@gmail.com', 'password':'sha1\$3b9a3cb5\$1\$78f98d5645e7fd08d6216cf8ad7ffc2ab686484d'})"
 docker exec -t mongodb mongo fablab --eval "db.contacts.insert({'firstName':'Vincent', 'lastName':'Chenal', 'email':'vinc.chenal@gmail.com'})"
 docker exec -t mongodb mongo fablab --eval "db.scheduled.insert({'day':'Lundi', 'open':'true','opening_morning':'9:00', 'closing_morning':'12:00', 'opening_afternoon':'14:00', 'closing_afternoon':'17:00'})"
 docker exec -t mongodb mongo fablab --eval "db.scheduled.insert({'day':'Mardi', 'open':'true','opening_morning':'9:00', 'closing_morning':'12:00', 'opening_afternoon':'14:00', 'closing_afternoon':'17:00'})"
